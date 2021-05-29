0811910a525c182e5a9b232596dfee0c3249015e
Jirawan.C <jirawan-chuapradit@users.noreply.github.com>
2021-05-29 19:53:14 +0700

    upload file in chunks with client streaming gRPC

--- cmd/client/main.go
+++ cmd/client/main.go
@@ -1,10 +1,13 @@
 package main
 
 import (
+	"bufio"
 	"context"
 	"flag"
 	"io"
 	"log"
+	"os"
+	"path/filepath"
 	"time"
 
 	"github.com/jirawan-chuapradit/grpc-golang-pcbook/pb"
@@ -26,19 +29,7 @@ func main() {
 	defer conn.Close()
 
 	laptopClient := pb.NewLaptopServiceClient(conn)
-
-	for i := 0; i < 10; i++ {
-		createLaptop(laptopClient)
-	}
-
-	filter := &pb.Filter{
-		MaxPriceUsd: 3000,
-		MinCpuCores: 4,
-		MinCpuGhz:   2.5,
-		MinRam:      &pb.Memory{Value: 8, Unit: pb.Memory_GIGABYTE},
-	}
-	searchLaptop(laptopClient, filter)
-
+	testUploadImage(laptopClient)
 }
 
 func searchLaptop(laptopClient pb.LaptopServiceClient, filter *pb.Filter) {
@@ -69,24 +60,19 @@ func searchLaptop(laptopClient pb.LaptopServiceClient, filter *pb.Filter) {
 		log.Print("\t+ name: ", laptop.GetName())
 		log.Print("\t+ cpu cores: ", laptop.GetCpu().GetNumberCores())
 		log.Print("\t+ cpu min ghz: ", laptop.GetCpu().GetMinGhz())
-		log.Print("\t+ ram: ",laptop.GetRam().GetValue(), laptop.GetRam().GetUnit())
-		log.Print("\t+ price: ",laptop.GetPriceUsd(), "usd")
+		log.Print("\t+ ram: ", laptop.GetRam().GetValue(), laptop.GetRam().GetUnit())
+		log.Print("\t+ price: ", laptop.GetPriceUsd(), "usd")
 	}
-
 }
 
-func createLaptop(laptopClient pb.LaptopServiceClient) {
-	log.Println("======> CreateNewLaptop")
+func createLaptop(laptopClient pb.LaptopServiceClient, laptop *pb.Laptop) {
+	req := &pb.CreateLaptopRequest{
+		Laptop: laptop,
+	}
 
 	// set time out
 	ctx, cancel := context.WithTimeout(context.Background(), 5*time.Second)
 	defer cancel()
-
-	laptop := sample.NewLaptop()
-
-	req := &pb.CreateLaptopRequest{
-		Laptop: laptop,
-	}
 
 	res, err := laptopClient.CreateLaptop(ctx, req)
 	if err != nil {
@@ -102,4 +88,89 @@ func createLaptop(laptopClient pb.LaptopServiceClient) {
 	}
 
 	log.Printf("created laptop with id: %s", res.Id)
+}
+
+func testCreateLaptop(laptopClient pb.LaptopServiceClient) {
+	createLaptop(laptopClient, sample.NewLaptop())
+}
+
+func testSearchLaptop(laptopClient pb.LaptopServiceClient) {
+	for i := 0; i < 10; i++ {
+		createLaptop(laptopClient, sample.NewLaptop())
+	}
+
+	filter := &pb.Filter{
+		MaxPriceUsd: 3000,
+		MinCpuCores: 4,
+		MinCpuGhz:   2.5,
+		MinRam:      &pb.Memory{Value: 8, Unit: pb.Memory_GIGABYTE},
+	}
+	searchLaptop(laptopClient, filter)
+}
+
+func testUploadImage(laptopClient pb.LaptopServiceClient){
+	laptop := sample.NewLaptop()
+	createLaptop(laptopClient, laptop)
+	uploadImage(laptopClient, laptop.GetId(), "tmp/laptop.jpg")
+}
+
+func uploadImage(laptopClient pb.LaptopServiceClient, laptopID string, imagePath string) {
+	file, err := os.Open(imagePath)
+	if err != nil {
+		log.Fatal("cannot open image file: ", err)
+	}
+	defer file.Close()
+
+	ctx, cancel := context.WithTimeout(context.Background(), 5*time.Second)
+	defer cancel()
+
+	stream, err := laptopClient.UploadImage(ctx)
+	if err != nil {
+		log.Fatal("cannot upload image: ", err)
+	}
+
+	req := &pb.UploadImageRequest{
+		Data: &pb.UploadImageRequest_Info{
+			Info: &pb.ImageInfo{
+				LaptopId: laptopID,
+				ImageType: filepath.Ext(imagePath),
+			},
+		},
+	}
+
+	err = stream.Send(req)
+	if err != nil {
+		log.Fatal("cannot send image info: ", err, stream.RecvMsg(nil))
+	}
+
+	reader := bufio.NewReader(file)
+	buffer := make([]byte, 1024)
+
+	for {
+		n, err := reader.Read(buffer)
+		if err == io.EOF{
+			break
+		}
+
+		if err != nil {
+			log.Fatal("cannot read chunk to buffer: ", err)
+		}
+
+		req := &pb.UploadImageRequest{
+			Data: &pb.UploadImageRequest_ChunkData{
+				ChunkData: buffer[:n],
+			},
+		}
+		err = stream.Send(req)
+		if err != nil {
+			log.Fatal("cannot send chunk to server: ", err, stream.RecvMsg(nil))
+		}
+	}
+
+	res, err := stream.CloseAndRecv()
+	if err != nil {
+		log.Fatal("cannot receive response: ", err)
+	}
+
+	log.Printf("image uploaded with id: %s, size: %d", res.GetId(), res.GetSize())
 }
